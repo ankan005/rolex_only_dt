b3256647eb29e3cafd37e866cb67996b53b9d2f6
Ankan005 <ankan005@gmail.com>
2019-03-22 16:00:31 +0000

    lineagify

--- lineage_rolex.mk
+++ lineage_rolex.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/xiaomi/rolex/full_rolex.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/havoc/config/common.mk)
+$(call inherit-product, vendor/lineage/config/full_phpne_common.mk)
 
-PRODUCT_NAME := havoc_rolex
+PRODUCT_NAME := lineage_rolex
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
